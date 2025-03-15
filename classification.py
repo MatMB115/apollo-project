f396e54700a9643f417549f16e2263274993d950
MatMB115 <MatMB115@users.noreply.github.com>
2025-03-15 19:35:39 -0300

    fix: adding top_k arg

--- classification.py
+++ classification.py
@@ -7,7 +7,7 @@ from sklearn.metrics import accuracy_score, f1_score, roc_auc_score, top_k_accur
 
 from utils import load_pickle_file, load_arguments, save_as_pickle
 
-def run_knn(X, y, kf, distance_metric="euclidean", k_values=range(1, 16)):
+def run_knn(X, y, kf, distance_metric="euclidean", k_values=range(1, 16), top_k=2):
     knn_results = {k: {"accuracy": [], "f1": [], "auc": [], "top_k": [], "y_true": [], "y_proba": []} for k in k_values}
 
     for train_idx, test_idx in kf.split(X):
@@ -24,14 +24,14 @@ def run_knn(X, y, kf, distance_metric="euclidean", k_values=range(1, 16)):
             knn_results[k]["accuracy"].append(accuracy_score(y_test, y_pred))
             knn_results[k]["f1"].append(f1_score(y_test, y_pred, average="macro"))
             knn_results[k]["auc"].append(roc_auc_score(y_test, y_pred_proba, multi_class="ovr"))
-            knn_results[k]["top_k"].append(top_k_accuracy_score(y_test, y_pred_proba, k=2))
+            knn_results[k]["top_k"].append(top_k_accuracy_score(y_test, y_pred_proba, k=top_k))
 
             knn_results[k]["y_true"].append(y_test)
             knn_results[k]["y_proba"].append(y_pred_proba)
 
     return knn_results
 
-def convert_to_dataframe(knn_results_euclidean, knn_results_cosine):
+def convert_to_dataframe(knn_results_euclidean, knn_results_cosine, top_k):
     rows = []
     for k in knn_results_euclidean.keys():
         rows.append({
@@ -39,11 +39,11 @@ def convert_to_dataframe(knn_results_euclidean, knn_results_cosine):
             "E-Accuracy": np.mean(knn_results_euclidean[k]["accuracy"]),
             "E-F1-Score": np.mean(knn_results_euclidean[k]["f1"]),
             "E-AUC": np.mean(knn_results_euclidean[k]["auc"]),
-            "E-Top_2": np.mean(knn_results_euclidean[k]["top_k"]),
+            f"E-Top_{top_k}": np.mean(knn_results_euclidean[k]["top_k"]),
             "C-Accuracy": np.mean(knn_results_cosine[k]["accuracy"]),
             "C-F1-Score": np.mean(knn_results_cosine[k]["f1"]),
             "C-AUC": np.mean(knn_results_cosine[k]["auc"]),
-            "C-Top_2": np.mean(knn_results_cosine[k]["top_k"])
+            f"C-Top_{top_k}": np.mean(knn_results_cosine[k]["top_k"])
         })
     
     df_results = pd.DataFrame(rows).set_index("K")
@@ -55,6 +55,11 @@ def main():
             "type": str,
             "default": "mini_gm_public_v0.1_processed.p",
             "help": "Full path (including the .p file) where the processed embeddings are located."
+        },
+        "top_k": {
+            "type": int,
+            "default": 2,
+            "help": "Value of K for Top-K Accuracy (default: 2)."
         }
     }
 
@@ -68,10 +73,10 @@ def main():
 
     kf = KFold(n_splits=10, shuffle=True, random_state=115)
 
-    knn_results_euclidean = run_knn(X, y, kf, distance_metric="euclidean")
-    knn_results_cosine = run_knn(X, y, kf, distance_metric="cosine")
+    knn_results_euclidean = run_knn(X, y, kf, distance_metric="euclidean",top_k=args.top_k)
+    knn_results_cosine = run_knn(X, y, kf, distance_metric="cosine", top_k=args.top_k)
 
-    df_results = convert_to_dataframe(knn_results_euclidean, knn_results_cosine)
+    df_results = convert_to_dataframe(knn_results_euclidean, knn_results_cosine, args.top_k)
 
     print("\nClassification Results Summary (Average Scores)")
     print(df_results.to_string())
